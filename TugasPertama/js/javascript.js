54b27f41ab856c281d410f540f99b50731d7d01b
Constantius Damar Wicaksono <constantiusdamar@gmail.com>
2014-08-31 20:07:51 +0700

    add keyboard function
    sprite moves when arrow key pressed

--- TugasPertama/js/javascript.js
+++ TugasPertama/js/javascript.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.font ="25px Arial";
+ctx.font = "25px Arial";
 document.body.appendChild(canvas);
 
 
@@ -16,13 +16,27 @@ player.onload = function () {
     ctx.drawImage(player, hero.x, hero.y);
 };
 
+var player2 = new Image();
+player2.src = "images/player2.png";
+player2.onload = function(){
+  ctx.drawImage(player2,hero2.x,hero2.y);
+};
+
+var hero2 ={
+    x: (canvas.width / 2)+20,
+    y: (canvas.height / 2) - 50,
+    speed: 256
+};
+
 var hero = {
-    x: (canvas.width / 2)-50.5,
-    y: (canvas.height / 2) -45.5 ,
+    x: (canvas.width / 2) - 26.5,
+    y: (canvas.height / 2) - 50,
     speed: 256
 
 };
 
+var mouse = {};
+var isMouseDown = false;
 var keysDown = {};
 
 window.addEventListener("keydown", function (e) {
@@ -32,6 +46,27 @@ window.addEventListener("keydown", function (e) {
 window.addEventListener("keyup", function (e) {
     delete keysDown[e.keyCode];
 });
+
+canvas.addEventListener("mousedown", function(){
+    isMouseDown = true;
+});
+
+canvas.addEventListener("mouseup", function(){
+    isMouseDown = false;
+});
+
+canvas.addEventListener("mousemove", function (event) {
+    mouse = getMousePosition(canvas, event);
+});
+
+function getMousePosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+};
+
 
 function update(modifier) {
     if (37 in keysDown) {
@@ -48,11 +83,20 @@ function update(modifier) {
     };
 };
 
+function updateMouse(){
+
+};
+
 function render() {
     ctx.drawImage(background, 0, 0);
     ctx.drawImage(player, hero.x, hero.y);
-    ctx.fillText(hero.x+" "+hero.y,10,30);
+    ctx.drawImage(player2, hero2.x, hero2.y);
+    ctx.fillText(hero.x + " " + hero.y, 10, 30);
+    ctx.fillText(mouse.x + " " + mouse.y, 10, 50);
+    ctx.fillText(isMouseDown,10,70);
 };
+
+
 
 
 function runTheGame() {
